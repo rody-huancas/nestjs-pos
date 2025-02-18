176620eb327e90508417d4106137d3f12be72737
Rody Huancas <rodyhuancas.04@gmail.com>
2025-02-17 23:05:25 -0500

    feat: enhance main application setup and add default SVG asset for improved UI

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
+import { join } from 'path';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useStaticAssets(join(__dirname, '../public'));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
